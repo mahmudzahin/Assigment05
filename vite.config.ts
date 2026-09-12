dd2561842926582eeeac46498e89814b9bc76e4d
mahmudzahin <mahmudzahin327@gmail.com>
2026-09-13 01:43:54 +0600

    Fix Vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss(),],
-  base: '/Assigment05/',
+  base: process.env.GITHUB_ACTIONS ? '/Assigment05/' : '/',
 })

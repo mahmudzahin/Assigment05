241ec77ff1c3f213706c89e1e904099ca7d0c24b
mahmudzahin <mahmudzahin327@gmail.com>
2026-09-13 01:03:19 +0600

    add the footer section

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import './index.css'
 import {Navbar} from  './compononts/Navbar/Navbar'
 import MainSection from './compononts/MainSection/MainSection'
 import Card from './compononts/Card/Card'
-
+import Footer from './compononts/Footer/Footer'
 function App() {
   return (
     <div>
       <Navbar/>
       <MainSection/>
       <Card/>
+      <Footer/>
     </div>
   )
 }
